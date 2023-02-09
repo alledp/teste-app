84c978cf7f907204b8cfb758a8f76bcf89916fdb
alledpaiva <alledpaiva@gmail.com>
2023-02-09 17:21:31 -0600

    feat: criacao do esqueleto do CRUD em UserRestController

--- lds-api/src/main/java/br/com/mobguide/controller/UserRestController.java
+++ lds-api/src/main/java/br/com/mobguide/controller/UserRestController.java
@@ -1,14 +1,43 @@
 package br.com.mobguide.controller;
 
+import br.com.mobguide.model.entities.UserModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "*")
 public class    UserRestController {
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable("id") final int id){
+        return ResponseEntity.ok(new UserModel());
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<List<UserModel>> getUsers() {
+        List<UserModel> list = Arrays.asList(new UserModel());
+        return ResponseEntity.ok(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") final int id){
+        return ResponseEntity.ok(body: true);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Integer> create(@RequestBody final UserModel user){
+        return ResponseEntity.ok(body: 1);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Boolean> update(@PathVariable("id") final int id,
+                                          @RequestBody()) {
+        return ResponseEntity.ok(body: true);
+    }
+
 
 }
